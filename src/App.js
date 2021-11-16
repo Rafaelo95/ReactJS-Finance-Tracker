4efc266fbe198516c9516520fa01ad5f669fa7ee
Rafaelo Antonov <ralf_antonov@abv.bg>
2021-11-16 14:22:42 +0200

    update
    navbar creation & styling in CSS - likely to be changed later

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/home/Home'
 import Login from './pages/login/Login'
 import Signup from './pages/signup/Signup'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Navbar/>
         <Routes>
           <Route exact path='/' element={<Home/>}/>
           <Route path='/login' element={<Login/>}/>
---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import { Link } from 'react-router-dom'
+
+// styles
+import './Navbar.css'
+
+export default function Navbar() {
+    return (
+        <nav className="navbar">
+            <ul className="navbar-ul">
+                <li className="nav-title"><Link to='/'>Finance Tracker</Link></li>
+
+                <li><Link to='/login'>Login</Link></li>
+                <li><Link to='/signup'>Signup</Link></li>
+            </ul>
+        </nav>
+    )
+}
