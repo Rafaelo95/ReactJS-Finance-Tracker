64e37cfb75751002b5cea5f4ac4a1d2dcd1e7a2b
Rafaelo Antonov <ralf_antonov@abv.bg>
2021-11-16 15:52:43 +0200

    creating signup page using same styles as login

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,7 +10,7 @@ export default function Navbar() {
                 <li className="nav-title"><Link to="/">Finance Tracker</Link></li>
 
                 <li><Link to="/login">Login</Link></li>
-                <li><Link to="/signup">Signup</Link></li>
+                <li><Link to="/signup">Sign Up</Link></li>
             </ul>
         </nav>
     )
--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,7 +1,40 @@
-import './Signup.css'
+import { useState } from "react";
+
+// styles
+import "./Signup.css";
 
 export default function Signup() {
-    return (
-        <div>Signup</div>
-    )
+  let [email, setEmail] = useState("")
+  let [password, setPassword] = useState("")
+  let [displayName, setDisplayName] = useState("")
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(email, password, displayName)
+  }
+
+  return (
+    <form className="signup-form" onSubmit={handleSubmit}>
+      <h3 className="signup-form-title">Sign Up</h3>
+      <label>Email:</label>
+      <input 
+        type="text"
+        onChange={(e) => setEmail(e.target.value)}
+      />
+
+      <label>Password:</label>
+      <input 
+        type="password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <label>Display Name:</label>
+      <input 
+        type="text" 
+        onChange={(e) => setDisplayName(e.target.value)}
+      />
+
+      <button>Register</button>
+    </form>
+  );
 }
