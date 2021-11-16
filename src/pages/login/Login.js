15b440ba2ece56f00d2a797ab3b7a5d89757d115
Rafaelo Antonov <ralf_antonov@abv.bg>
2021-11-16 15:21:46 +0200

    create login page + css to it

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,14 +1,31 @@
+import { useState } from "react"
+
+// styles
 import "./Login.css";
 
 export default function Login() {
+  let [email, setEmail] = useState('')
+  let [password, setPassword] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(email, password)
+  }
+
   return (
-    <form className="login-form">
+    <form className="login-form" onSubmit={handleSubmit}>
       <h3 className="login-form-title">Login</h3>
       <label>Email:</label>
-      <input type="text"></input>
+      <input 
+        type="text"
+        onChange={(e) => setEmail(e.target.value)}
+      />
 
       <label>Password:</label>
-      <input type="password"></input>
+      <input 
+        type="password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
 
       <button>Log in</button>
     </form>
