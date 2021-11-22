2c3c416b287af29394d4d784c78237f6490086cf
Rafaelo Antonov <ralf_antonov@abv.bg>
2021-11-22 15:11:48 +0200

    useFirebase hook to create/delete elements

---
+++ src/hooks/useFirebase.js
@@ -0,0 +1,62 @@
+import { useReducer, useEffect, useState } from "react"
+import { db } from "../firebase/config"
+
+let initialState = {
+  isPending: false,
+  error: null,
+  success: null,
+}
+
+const firestoreReducer = (state, action) => {
+  switch (action.type) {
+    case "IS_PENDING":
+      return {success: false, isPending: true, error: null}
+    case "ERROR":
+      return {success: false, isPending: false, error: action.payload}
+    case "ADDED_DOCUMENT":
+      return {success: true, isPending: false, error: null}
+    default:
+      return state
+  }
+}
+
+export const useFirestore = (collection) => {
+  const [response, dispatch] = useReducer(firestoreReducer, initialState)
+  const [isCancelled, setIsCancelled] = useState(false)
+
+  // collection ref
+  const ref = db.collection(collection)
+
+  // only dispatch if not cancelled
+  const dispatchIfNotCancelled = (action) => {
+    if (!isCancelled) {
+      dispatch(action)
+    }
+  }
+  
+  // add a document
+  const addDocument = async (doc) => {
+    dispatch({ type: "IS_PENDING" })
+
+    try {
+      const addedDocument = await ref.add({...doc})
+      dispatchIfNotCancelled({ type: "ADDED_DOCUMENT", payload: addedDocument })
+    }
+    catch (err) {
+      dispatchIfNotCancelled({ type: "ERROR", payload: err.message })
+    }
+
+  }
+
+  // delete a document
+  const deleteDocument = async (doc) => {
+
+  }
+
+  useEffect(() => {
+    return () => setIsCancelled(true)
+  }, [])
+
+  return { addDocument, deleteDocument, response }
+
+}
