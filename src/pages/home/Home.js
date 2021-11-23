292b6bd07ac3b15781bf13ce95ecb4ed85bffb99
Rafaelo Antonov <ralf_antonov@abv.bg>
2021-11-23 21:33:01 +0200

    Update Home.js

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,17 +1,18 @@
-import { TransactionForm } from "./TransactionForm"
+import { TransactionForm } from "./TransactionForm";
+import { AuthContext } from "../../contexts/AuthContext";
+import { useContext } from "react";
 
 // styles
-import "./Home.css"
+import "./Home.css";
 
 export default function Home() {
-    return (
-      <div className="container">
-        <div className="content">
-          transaction list
-        </div>
-        <div className="sidebar">
-          <TransactionForm />
-        </div>
+  const { user } = useContext(AuthContext);
+  return (
+    <div className="container">
+      <div className="content">transaction list</div>
+      <div className="sidebar">
+        <TransactionForm uid={user.uid} />
       </div>
-    )
-  }
+    </div>
+  );
+}
